5637947a55df0648415e420d3c053de8a1fbea88
Lennart Kimmel <lennartkimmel@gmail.com>
2019-01-07 22:59:47 +0100

    Made inputDb code readable

---
+++ includes/Connectie_db.php
@@ -0,0 +1,18 @@
+<?php
+//Require DB settings with connection variable
+require_once "connectToDb.php";
+
+//Get the result set from the database with a SQL query
+$query = "SELECT * FROM reserveringen";
+
+$result = mysqli_query($db, $query)
+or die('Error'. mysqli_error($db));
+
+//Loop through the result to create a custom array
+$reservation = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $reservation[] = $row;
+}
+
+//Close connection
+mysqli_close($db);
--- inputDb.php
+++ inputDb.php
@@ -2,7 +2,8 @@
 // hiermee koppel je dit .php bestand aan de database
 include_once 'includes/connectToDb.php';
 
-// Hiermee koppel je F
+$errors = [];
+
 if (isset($_POST['submit'])) {
     //Postback with the data showed to the user, first retrieve data from 'Super global'
     $firstName = $_POST['Voornaam'];
@@ -12,7 +13,33 @@ if (isset($_POST['submit'])) {
     $phoneNumber = $_POST['Telefoon'];
     $visitorsPass = $_POST['Bezoekerspas'];
 
-    //Check if data is valid & generate error if not so
+
+    $errors = getErrorsForEmptyFields($firstName, $lastName, $street, $email, $phoneNumber, $visitorsPass);
+
+    $hasErrors = !empty($errors);
+
+    if (!$hasErrors) {
+        insertReservationIntoDatabase($db, $firstName, $lastName, $street, $email, $phoneNumber, $visitorsPass);
+    }
+}
+
+function showErrorsOnClientSide ($errors) {
+    $hasErrors = !empty($errors);
+    if ($hasErrors) {
+        echoErrorsAsHtml($errors);
+    }
+}
+
+function echoErrorsAsHtml ($errors) {
+    echo "<ul class='errors'>";
+
+    foreach  ($errors as $error) {
+        echo "<li>{$error}</li>";
+    }
+    echo "</ul>";
+}
+
+function getErrorsForEmptyFields($firstName, $lastName, $street, $email, $phoneNumber, $visitorsPass) {
     $errors = [];
     if ($firstName == "") {
         $errors[] = 'Voornaam moet ingevuld worden';
@@ -32,28 +59,25 @@ if (isset($_POST['submit'])) {
     if ($visitorsPass == "") {
         $errors[] = 'Bezoekerspas moet ingevuld worden';
     }
-    try{
-        $query = $db->prepare("
-        INSERT INTO `reserveringen` (`Voornaam`, `Achternaam`, `Straatnaam`, `Email`, `Telefoon`, `Bezoekerspas`) VALUES (:Voornaam, :Achternaam, :Straatnaam, :Email, :Telefoon, :Bezoekerspas)");
-        $query->bindParam(':Voornaam', $firstName);
-        $query->bindParam(':Achternaam', $lastName);
-        $query->bindParam(':Straatnaam', $street);
-        $query->bindParam(':Email', $email);
-        $query->bindParam(':Telefoon', $phoneNumber);
-        $query->bindParam(':Bezoekerspas', $visitorsPass);
-        $query->execute();
-    }
-    catch (PDOException $e) {
-        $sMsg = '<p>
-            Line: '.$e->getLine().'<br />
-            File: '.$e->getFile().'<br />
-            Message: '.$e->getMessage().'
-        </p>';
-
-        trigger_error($sMsg);
-    }
+    return $errors;
 }
+
+function insertReservationIntoDatabase($db, $firstName, $lastName, $street, $email, $phoneNumber, $visitorsPass) {
+
+    $query = $db->prepare("
+        
+        INSERT INTO `reserveringen` (`Voornaam`, `Achternaam`, `Straatnaam`, `Email`, `Telefoon`, `Bezoekerspas`) VALUES (:Voornaam, :Achternaam, :Straatnaam, :Email, :Telefoon, :Bezoekerspas)");
+    $query->bindParam(':Voornaam', $firstName);
+    $query->bindParam(':Achternaam', $lastName);
+    $query->bindParam(':Straatnaam', $street);
+    $query->bindParam(':Email', $email);
+    $query->bindParam(':Telefoon', $phoneNumber);
+    $query->bindParam(':Bezoekerspas', $visitorsPass);
+    $query->execute();
+}
+
 ?>
+
 <!doctype html>
 <html>
 <head>
@@ -62,47 +86,48 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
-<h1>Maak uw reservatie hier!</h1>
-<?php if (isset($errors)) { ?>
-    <ul class="errors">
-        <?php foreach ($errors as $error) { ?>
-            <li><?= $error; ?></li>
-        <?php } ?>
-    </ul>
-<?php } ?>
+<section class="sectionHeader">
+    <h1 class="reservationHeader">Maak uw reservatie hier!</h1>
+</section>
 
-<form action="<?= $_SERVER['REQUEST_URI']; ?>" method="post">
-    <div class="data-field">
-        <label for="Voornaam">Voornaam</label>
-        <input id="Voornaam" type="text" name="Voornaam" value="<?= (isset($firstName) ? $firstName : ''); ?>"/>
-        <span><?= (isset($errors['Voornaam']) ? $errors['Voornaam'] : '') ?></span>
-    </div>
-    <div class="data-field">
-        <label for="Achternaam">Achternaam</label>
-        <input id="Achternaam" type="text" name="Achternaam" value="<?= (isset($lastName) ? $lastName : ''); ?>"/>
-    </div>
-    <div class="data-field">
-        <label for="Straatnaam">Straatnaam</label>
-        <input id="Straatnaam" type="text" name="Straatnaam" value="<?= (isset($street) ? $street : ''); ?>"/>
-    </div>
-    <div class="data-field">
-        <label for="Email">Email</label>
-        <input id="Email" type="email" name="Email" value="<?= (isset($email) ? $email : ''); ?>"/>
-    </div>
-    <div class="data-field">
-        <label for="Telefoon">Telefoonnummer</label>
-        <input id="Telefoon" type="number" name="Telefoon" value="<?= (isset($phoneNumber) ? $phoneNumber : ''); ?>"/>
-    </div>
-    <div class="data-field">
-        <label for="Bezoekerspas">Bezoekerspas</label>
-        <input id="Bezoekerspas" type="text" name="Bezoekerspas" value="<?= (isset($visitorsPass) ? $visitorsPass : ''); ?>"/>
-    </div>
-    <div class="data-submit">
-        <input type="submit" name="submit" value="Save"/>
-    </div>
-</form>
-<div>
-    <a href="index.php">Ga terug naar home</a>
-</div>
+<?php
+showErrorsOnClientSide($errors);
+?>
+
+<!--in een FORM moeten altijd meerdere attributen zitten zoals: type (verplicht bij HTML)  -> name (Verplicht bij php anders niet opgenomen in PHP)-> value-->
+<section class="sectionBody">
+    <form action="<?= $_SERVER['REQUEST_URI']; ?>" method="post">
+        <div class="data-field">
+            <label for="Voornaam" class="labels">Voornaam</label>
+            <input class="inputField" type="text" name="Voornaam" placeholder="Voornaam" value="<?= (isset($firstName) ? $firstName : ''); ?>"/>
+        </div>
+        <div class="data-field">
+            <label for="Achternaam" class="labels">Achternaam</label>
+            <input class="inputField" type="text" name="Achternaam" placeholder="Achternaam" value="<?= (isset($lastName) ? $lastName : ''); ?>"/>
+        </div>
+        <div class="data-field">
+            <label for="Straatnaam" class="labels">Straatnaam</label>
+            <input class="inputField" type="text" name="Straatnaam" placeholder="Straatnaam" value="<?= (isset($street) ? $street : ''); ?>"/>
+        </div>
+        <div class="data-field">
+            <label for="Email">Email</label>
+            <input class="inputField" type="email" name="Email" placeholder="Email" value="<?= (isset($email) ? $email : ''); ?>"/>
+        </div>
+        <div class="data-field">
+            <label for="Telefoon">Telefoonnummer</label>
+            <input class="inputField" type="number" name="Telefoon" placeholder="Telefoonnummer" value="<?= (isset($phoneNumber) ? $phoneNumber : ''); ?>"/>
+        </div>
+        <div class="data-field">
+            <label for="Bezoekerspas">Bezoekerspas</label>
+            <input class="inputField" type="text" name="Bezoekerspas" placeholder="Bezoekerspas" value="<?= (isset($visitorsPass) ? $visitorsPass : ''); ?>"/>
+        </div>
+        <div class="data-submit">
+            <input type="submit" name="submit" value="Save"/>
+        </div>
+    </form>
+</section>
+<!--<div>-->
+<!--    <a href="index.php">Ga terug naar home</a>-->
+<!--</div>-->
 </body>
 </html>
